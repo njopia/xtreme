87d6034b883333c97b70f0a2ef84aaf3f64acb93
Natan Jopia <jopia.natan@gmail.com>
2026-05-15 22:18:37 -0400

    fix: corregir importación de GameDig para asegurar compatibilidad

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import { GameDig } from 'gamedig';
+import  GameDig from 'gamedig';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
